ed19e7dc1f6e7edb0589f976b3ed3b0a7514e80a
Mikhailrs <msghost771@gmail.com>
2023-01-28 05:46:40 +0300

    Fix scope sorted in message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
-  scope :sorted, -> { order(id: :desc) }
+  scope :sorted, -> { order(:id) }
 
   validates presence: true
 end
